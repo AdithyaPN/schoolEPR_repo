a770f9271ea5cb281f5a5b9df158167247392c8e
Adithya <adithyapn98@gmail.com>
2022-09-21 12:41:46 +0530

    page loading

--- school_EPR/school_EPR/urls.py
+++ school_EPR/school_EPR/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('schoolAdmin/',include('schoolAdmin.urls'))
+    path('',include('schoolAdmin.urls'))
 ]
