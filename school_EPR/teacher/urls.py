e743faf8acb6d5bcda9129adbb751adc553d77dc
Adithya <adithyapn98@gmail.com>
2022-09-24 21:09:15 +0530

    teacher functionalities added

--- school_EPR/teacher/urls.py
+++ school_EPR/teacher/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns=[
     path('home', views.teacher_home),
+    path('addstudent', views.add_student),
+    path('viewstudent', views.view_student),
+    path('changepassword', views.change_password),
 ]
--- school_EPR/teacher/views.py
+++ school_EPR/teacher/views.py
@@ -5,3 +5,12 @@ import os
 # Create your views here.
 def teacher_home(request):
     return render(request, 'teachertemplates/home.html')
+
+def add_student(request):
+    return render(request, 'teachertemplates/addstudent.html')
+
+def view_student(request):
+    return render(request, 'teachertemplates/viewstudent.html')
+
+def change_password(request):
+    return render(request, 'teachertemplates/changepassword.html')
